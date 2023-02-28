0b27479c0506f84c23a84e30c477713205eec3fb
vinod <vinodpr737947@gmail.com>
2023-02-28 23:41:01 +0530

    complete header

--- frontend/src/header/Header.jsx
+++ frontend/src/header/Header.jsx
@@ -1,19 +1,59 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./Header.css";
 
+import {BsYoutube} from "react-icons/bs"
+import {AiOutlineMenu} from "react-icons/ai"
+import {FiSearch} from "react-icons/fi"
+import {MdVideoCall} from "react-icons/md"
+import {IoMdNotifications} from "react-icons/io"
+import userimage from "../assets/image1.jpg";
+
+
+
 const Header = () => {
+
+  const [inputValue, setInputValue] = useState("");
+   
+  const handleChange=(e)=>{
+     e.preventDefault();
+     setInputValue(e.target.value);
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(inputValue);
+  }
+
   return (
     <div className='header_container'>
        <div className="logo-menu">
-        a
+         <AiOutlineMenu/>
+         <div className="logo-name">
+          <BsYoutube/><span>YouTube</span><sup>IN</sup>
+         </div>
        </div>
       
        <div className="searchbar">
-        b
+         <form onSubmit={(e)=>handleSubmit(e)}>
+           <input type="text" placeholder='Search' onChange={(e)=>handleChange(e)} />
+           <button type='submit'><FiSearch/></button>
+         </form>
        </div>
 
        <div className="profile-menu">
-        c
+         
+           <div>
+              <MdVideoCall/>
+           </div>
+           
+           <div>
+             <IoMdNotifications/>
+           </div>
+           
+           <div>
+             <img src={userimage} alt="user_image" />
+           </div>
+         
        </div>
 
 
